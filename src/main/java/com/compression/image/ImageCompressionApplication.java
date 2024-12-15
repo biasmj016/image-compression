8bf476c7384d6416c5a62274b402e79f6d45fee2
biasmj016 <mjkim031@gmail.com>
2024-12-15 23:43:16 +0900

    project init

---
+++ src/main/java/com/compression/image/ImageCompressionApplication.java
@@ -0,0 +1,13 @@
+package com.compression.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageCompressionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageCompressionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/compression/image/ImageCompressionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compression.image;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageCompressionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
