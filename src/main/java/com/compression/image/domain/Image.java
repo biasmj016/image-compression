1c8dbb17f176e2aa30576894263806e1b7e97e3f
biasmj016 <mjkim031@gmail.com>
2024-12-16 23:26:51 +0900

    유효 확장자 검증 코드 추가

--- src/main/java/com/compression/image/domain/Image.java
+++ src/main/java/com/compression/image/domain/Image.java
@@ -2,11 +2,15 @@ package com.compression.image.domain;
 
 import com.compression.image.infrastructure.out.FileUploader;
 
+import java.util.List;
+
 public record Image (
         FileUploader files,
         String format,
         int quality
 ) {
+    private static final List<String> VALID_FORMATS = List.of("jpg", "png");
+
     public Image {
         if (quality < 0 || quality > 100) {
             throw new IllegalArgumentException("Compression quality must be between 0 and 100.");
@@ -16,6 +20,9 @@ public record Image (
         }
         if (format == null || format.isBlank()) {
             throw new IllegalArgumentException("Format must be specified.");
+        }
+        if (!VALID_FORMATS.contains(format.toLowerCase())) {
+            throw new IllegalArgumentException("Format must be either 'jpg' or 'png'.");
         }
     }
 }
